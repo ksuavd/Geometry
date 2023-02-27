9146b9ab0242ab8ef8b601422a182b1cc8529851
ksuavd <eva2625@mail.ru>
2023-02-27 23:11:03 +0500

    Добавьте файлы проекта.

---
+++ Geometry/Geometry.cpp
@@ -0,0 +1,11 @@
+﻿// Geometry.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, " ");
+}
+
